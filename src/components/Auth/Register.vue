00831ec4867a56332ef0f1551975a928a57a6158
podyankee <podyankee@gmail.com>
2019-04-13 09:59:08 +0300

    register page creeated

--- src/components/Auth/Register.vue
+++ src/components/Auth/Register.vue
@@ -1,14 +1,52 @@
 <template lang = "pug">
-	v-container
-		v-layout(row)
-			v-flex(xs12)
-				h1 Register
+	v-container(fluid='' fill-height='')
+		v-layout(align-center='' justify-center='')
+			v-flex(xs12='' sm8='' md6='')
+				v-card.elevation-12
+					v-toolbar(dark='' color='primary')
+						v-toolbar-title Registration form
+					v-card-text
+						v-form(ref='form', v-model='valid', lazy-validation='')
+							v-text-field(prepend-icon='person' name='email' aria-label='Email' type='email' :rules="emailRules" v-model='email')
+							v-text-field(prepend-icon='lock' name='password' aria-label='Password' type='password' :rules="passwordRules" v-model='password' :counter="6")
+							v-text-field(prepend-icon='repeat' name='confirm-password' aria-label='Confirm Password' type='password' :rules="confirmPasswordRules" v-model='confipmPassword' :counter="6")
+					v-card-actions
+						v-spacer
+						v-btn(color='primary' @click='onSubmit' :disabled="!valid") Create  Account
 </template>
 
 <script>
 export default {
 	data () {
-		return {}
+		return {
+			email: '',
+			password: '',
+			confipmPassword: '',
+			valid: false,
+			emailRules: [
+				v => !!v || 'Email is required',
+				v => /.+@.+/.test(v) || 'Email mast be valid'
+			],
+			passwordRules: [
+				v => !!v || 'Password is required',
+				v => (v && v.length >= 6) || 'Password must be equal or more than 6 characters'
+			],
+			confirmPasswordRules: [
+				v => !!v || 'Password is required',
+				v => v === this.password || 'Password should match'
+			]
+		}
+	},
+	methods: {
+		onSubmit () {
+			if (this.$refs.form.validate()) {
+				const user = {
+					email: this.email,
+					password: this.password
+				}
+				console.log(user)
+			}
+		}
 	}
 }
 </script>
